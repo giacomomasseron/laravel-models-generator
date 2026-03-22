871a7c95a7093d65bacf1c8542c9bf456370426b
Giacomo Masseroni <kylnas@tiscali.it>
2026-03-22 10:29:44 +0100

    feat: to laravel 13

---
+++ src/Writers/Factory/Laravel13/Writer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GiacomoMasseroni\LaravelModelsGenerator\Writers\Factory\Laravel13;
+
+use GiacomoMasseroni\LaravelModelsGenerator\Concerns\Factory\Laravel9\HasFields;
+use GiacomoMasseroni\LaravelModelsGenerator\Writers\Factory\WriterInterface;
+
+class Writer extends \GiacomoMasseroni\LaravelModelsGenerator\Writers\Factory\Writer implements WriterInterface
+{
+    use HasFields;
+}
